c2871aa0217651ceb9cecc3997ae6826e64418d6
Marko Black <marko.blck@gmail.com>
2010-12-29 18:20:54 +0200

    added initial C# UI project

---
+++ SComUI/SComUI/SComUIForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SComUI
+{
+    public partial class SComUIForm : Form
+    {
+        public SComUIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
